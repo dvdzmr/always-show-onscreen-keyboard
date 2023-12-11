6ea59df8e1a593a661c572eabd947f9b7b61bd42
David <51931592+dvdzmr@users.noreply.github.com>
2023-12-10 22:33:08 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -4,13 +4,13 @@ import Clutter from 'gi://Clutter';
 export default class OnScreenKeyboard {
 
     enable() {
-        this.defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
-        this.touchMode = Clutter.get_default_backend().get_default_seat();       
-		this.touchMode.get_touch_mode = () => true;
+	    this.defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
+	    this.touchMode = Clutter.get_default_backend().get_default_seat();       
+	    this.touchMode.get_touch_mode = () => true;
 	}
 	
 	disable() {
-        // Nullifying touchmode by setting it back to default:
+		// Nullifying touchmode by setting it back to default:
 		this.touchMode.get_touch_mode = this.defaultMode;
 		this.touchMode = null;
 	}
