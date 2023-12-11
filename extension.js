22eb725d895fd3ac215e79fbca91d3fe63ebb8cb
dvdzmr <51931592+dvdzmr@users.noreply.github.com>
2023-12-10 22:07:34 -0500

    Nullified touchmode in disable()

--- extension.js
+++ extension.js
@@ -1,15 +1,12 @@
 import Clutter from 'gi://Clutter';
 
-let defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
-
 export default class OnScreenKeyboard {
-	touchMode = Clutter.get_default_backend().get_default_seat();
-	
     enable() {
-		this.touchMode.get_touch_mode = () => true;
-	}
-	
-	disable() {
-		this.touchMode.get_touch_mode = defaultMode;
-	}
+        this.touchMode = Clutter.get_default_backend().get_default_seat();
+        this.touchMode.get_touch_mode = () => true;
+    }
+
+    disable() {
+        this.touchMode = null;
+    }
 }
