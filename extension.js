377e810dbb3b48069e0be73327ee5f0209e72f40
dvdzmr <51931592+dvdzmr@users.noreply.github.com>
2023-12-10 22:28:05 -0500

    Nullified touchmode properly.

--- extension.js
+++ extension.js
@@ -1,12 +1,17 @@
 import Clutter from 'gi://Clutter';
 
-export default class OnScreenKeyboard {
-    enable() {
-        this.touchMode = Clutter.get_default_backend().get_default_seat();
-        this.touchMode.get_touch_mode = () => true;
-    }
 
-    disable() {
-        this.touchMode = null;
-    }
+export default class OnScreenKeyboard {
+
+    enable() {
+        this.defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
+        this.touchMode = Clutter.get_default_backend().get_default_seat();       
+		this.touchMode.get_touch_mode = () => true;
+	}
+	
+	disable() {
+        // Nullifying touchmode by setting it back to default:
+		this.touchMode.get_touch_mode = this.defaultMode;
+		this.touchMode = null;
+	}
 }
