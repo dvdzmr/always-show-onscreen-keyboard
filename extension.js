1c64f41993d972866f431f06cc7d5252a6f5794b
David <51931592+dvdzmr@users.noreply.github.com>
2023-12-10 22:33:54 -0500

    Fixed faulty indentation

--- extension.js
+++ extension.js
@@ -2,11 +2,10 @@ import Clutter from 'gi://Clutter';
 
 
 export default class OnScreenKeyboard {
-
-    enable() {
-	    this.defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
-	    this.touchMode = Clutter.get_default_backend().get_default_seat();       
-	    this.touchMode.get_touch_mode = () => true;
+	enable() {
+		this.defaultMode = Clutter.get_default_backend().get_default_seat().get_touch_mode;
+		this.touchMode = Clutter.get_default_backend().get_default_seat();       
+		this.touchMode.get_touch_mode = () => true;
 	}
 	
 	disable() {
